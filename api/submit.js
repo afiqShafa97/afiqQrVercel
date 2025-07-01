36bcf0fc8a7fa1a5e2e3289f1ff2e7c91e1cc69e
Afiq <taufiqrahman86@gmail.com>
2025-07-01 08:59:31 +0700

    bugfix fercel, added font type file

--- api/submit.js
+++ api/submit.js
@@ -1,13 +1,10 @@
 import { imageSync } from "qr-image";
 import sharp from "sharp";
+import fs from "fs";
+import path from "path";
 
 function escapeXML(text) {
-  return text
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&apos;");
+  return text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
 }
 
 async function generateQR(tautan) {
@@ -16,12 +13,28 @@ async function generateQR(tautan) {
 }
 
 async function createTextImage(textContent, width) {
-  const limitedText = textContent.slice(0, 30);
-  const safeText = escapeXML(limitedText);
-  const svg = `<svg width="${width}" height="30" xmlns="http://www.w3.org/2000/svg">
-      <rect width="100%" height="100%" fill="white"/>
-      <text x="50%" y="0" font-size="16" fill="black" text-anchor="middle" dominant-baseline="hanging">${safeText}</text>
-    </svg>`;
+  const limitedText = escapeXML(textContent.slice(0, 30));
+  const fontPath = path.resolve("./fonts/OpenSans-Regular.ttf");
+  const fontBase64 = fs.readFileSync(fontPath).toString("base64");
+
+  const svg = `<svg xmlns='http://www.w3.org/2000/svg' width='${width}' height='30'>
+    <defs>
+      <style type='text/css'>
+        @font-face {
+          font-family: 'MyFont';
+          src: url("data:font/ttf;base64,${fontBase64}");
+        }
+        text {
+          font-family: 'MyFont';
+          font-size: 16px;
+          fill: black;
+        }
+      </style>
+    </defs>
+    <rect width='100%' height='100%' fill='white'/>
+    <text x='50%' y='5' dominant-baseline='hanging' text-anchor='middle'>${limitedText}</text>
+  </svg>`;
+
   return Buffer.from(svg);
 }
 
@@ -35,12 +48,12 @@ async function combineImages(qrBuffer, textBuffer) {
       width: qrMeta.width,
       height: qrMeta.height + 30,
       channels: 3,
-      background: "white",
-    },
+      background: "white"
+    }
   })
     .composite([
       { input: await qrImage.toBuffer(), top: 0, left: 0 },
-      { input: await textImage.toBuffer(), top: qrMeta.height, left: 0 },
+      { input: await textImage.toBuffer(), top: qrMeta.height, left: 0 }
     ])
     .png()
     .toBuffer();
@@ -51,18 +64,17 @@ async function combineImages(qrBuffer, textBuffer) {
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { tautan, deskripsi } = req.body;
-    //console.log(deskripsi);
+
     const qrBuffer = await generateQR(tautan);
     let finalImage;
+
     if (deskripsi) {
       const qrMeta = await sharp(qrBuffer).metadata();
       const textBuffer = await createTextImage(deskripsi, qrMeta.width);
       finalImage = await combineImages(qrBuffer, textBuffer);
-    } else {
-        finalImage = qrBuffer;
     }
 
-    const base64 = Buffer.from(finalImage).toString("base64");
+    const base64 = Buffer.from(finalImage || qrBuffer).toString("base64");
     res.status(200).json({ base64 });
   } else {
     res.status(405).json({ message: "Method Not Allowed" });
